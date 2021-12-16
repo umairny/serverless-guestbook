a054ea14325647236e41f50c948876e918c27449
umairny <67115914+umairny@users.noreply.github.com>
2021-12-16 14:27:53 -0500

    Updated url for api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://04406b27.us-south.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
